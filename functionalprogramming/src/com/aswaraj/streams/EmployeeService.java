5ab340052f05ac7bea603b6f225019ad7114769a
aswar <aswar@LAPTOP-U6CRN832>
2019-10-24 22:17:56 +0530

    New methods added.

--- functionalprogramming/src/com/aswaraj/streams/EmployeeService.java
+++ functionalprogramming/src/com/aswaraj/streams/EmployeeService.java
@@ -1,10 +1,11 @@
 package com.aswaraj.streams;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java.util.Optional;import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class EmployeeService {
@@ -14,7 +15,7 @@ public class EmployeeService {
 	public void addEmployee() {
 		employeeList = new ArrayList<Employee>();
 		employeeList.add(new Employee(111, "Jiya Brein", 32, "Female", "HR", 2011, 25000.0));
-		employeeList.add(new Employee(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 2015, 13500.0));
+		employeeList.add(new Employee(122, "Paul Niksui", 25, "Male", "Sales And Marketing", 1998, 13500.0));
 		employeeList.add(new Employee(133, "Martin Theron", 29, "Male", "Infrastructure", 2012, 18000.0));
 		employeeList.add(new Employee(144, "Murali Gowda", 28, "Male", "Product Development", 2014, 32500.0));
 		employeeList.add(new Employee(155, "Nima Roy", 27, "Female", "HR", 2013, 22700.0));
@@ -31,7 +32,21 @@ public class EmployeeService {
 		employeeList.add(new Employee(266, "Sanvi Pandey", 26, "Female", "Product Development", 2015, 28900.0));
 		employeeList.add(new Employee(277, "Anuj Chettiar", 31, "Male", "Product Development", 2012, 35700.0));
 	}
+	
+	private void printEmployeeDetails(Employee employee) {
+		System.out.println("==============================================================");
+		System.out.println("ID: "+ employee.getId());
+		System.out.println("Name: "+ employee.getName());
+		System.out.println("Gender: "+ employee.getGender());
+		System.out.println("Department: "+ employee.getDepartment());
+		System.out.println("Year of Joining: "+ employee.getYearOfJoining());
+		System.out.println("Salary: "+ employee.getSalary());
+		System.out.println();
+	}
 
+	/**
+	 *  Count Employees by Gender.
+	 */
 	public void countByGender() {
 		Map<String, Long> noOfMaleAndFemaleEmployees = 
 				employeeList.stream()
@@ -39,6 +54,10 @@ public class EmployeeService {
 		System.out.println(noOfMaleAndFemaleEmployees);
 	}
 
+	
+	/**
+	 * Print all the Departments.
+	 */
 	public void printDepartments() {
 		employeeList.stream()
 			.map(Employee::getDepartment)
@@ -46,6 +65,9 @@ public class EmployeeService {
 			.forEach(System.out::println);
 	}
 	
+	/**
+	 * Calculate average age Gender wise.
+	 */
 	public void printAvgAgeByGender() {
 		Map<String, Double> avgGenderAge = 
 				employeeList.stream()
@@ -53,6 +75,9 @@ public class EmployeeService {
 		System.out.println(avgGenderAge);
 	}
 	
+	/**
+	 * Fetches the details of highest paid Employee.
+	 */
 	public void getHighestPaidEmployee() {
 		Optional<Employee> highestPaidEmployeeWrapper =
 				employeeList.stream()
@@ -62,6 +87,9 @@ public class EmployeeService {
 		System.out.println(highestPaidEmployee.toString());
 	}
 	
+	/**
+	 * Fetches details of Employee who has joined after 2015.
+	 */
 	public void filterEmployeeByJoiningDate() {
 		employeeList.stream()
 			.filter(e -> e.getYearOfJoining() > 2015)
@@ -69,6 +97,9 @@ public class EmployeeService {
 			.forEach(System.out::println);
 	}
 	
+	/**
+	 * Counts no. of Employees, Department wise.
+	 */
 	public void countByDepartment() {
 		employeeList.stream()
 			.collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()))
@@ -78,12 +109,101 @@ public class EmployeeService {
 				});
 	}
 	
+	/**
+	 * Fetches average salary Department wise.
+	 */
 	public void printAvgSalaryByDepartment() {
 		employeeList.stream()
 			.collect(Collectors.groupingBy(Employee::getDepartment, Collectors.averagingDouble(Employee::getSalary)))
 			.entrySet()
 			.forEach(entry -> {
 				System.out.println(entry.getKey() + ": " + entry.getValue());
+			});
+	}
+	
+	/**
+	 * Fetches youngest Employee of Product Development Department.
+	 */
+	public void filterYoungestEmployee() {
+		Optional<Employee> youngestEmployeeWrapper = employeeList.stream()
+			.filter(e -> e.getDepartment().equals("Product Development") && e.getGender().equals("Male"))
+			.min(Comparator.comparingInt(Employee::getAge));
+		
+		Employee employee = youngestEmployeeWrapper.get();
+		printEmployeeDetails(employee);
+	}
+	
+	/**
+	 * Fetches senior most Employee in the Organization.
+	 */
+	public void seniorMostEmployee() {
+		Optional<Employee> seniorMostEmployeeWrapper = employeeList.stream()
+			.sorted(Comparator.comparingInt(Employee::getYearOfJoining))
+			.findFirst();
+		printEmployeeDetails(seniorMostEmployeeWrapper.get());
+	}
+	
+	
+	/**
+	 * Groups Gender and counts Employees.
+	 */
+	public void employeesByDepartments() {
+		Map<String, Long> employeesByDepartmentsMap = employeeList.stream()
+			.filter(e -> e.getDepartment().equals("Sales And Marketing"))
+			.collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+		
+		System.out.println(employeesByDepartmentsMap);
+	}
+	
+	/**
+	 * Fetches average salary Gender wise.
+	 */
+	public void averageSalaryByGender() {
+		Map<String, Double> averageSalaryByGenderMap = employeeList.stream()
+				.collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingDouble(Employee::getSalary)));
+		
+		System.out.println(averageSalaryByGenderMap);		
+	}
+	
+	public void getAllEmployeesByDepartment() {
+		employeeList.stream()
+			.collect(Collectors.groupingBy(Employee::getDepartment))
+			.entrySet()
+			.forEach(entry -> 
+						{
+							System.out.println(entry.getKey());
+							System.out.println("----------------------------------------------------------------");
+							entry.getValue()
+								.forEach(e -> printEmployeeDetails(e));
+						}	
+					);
+	}
+	
+	public void updateEmployeeDepartment() {
+		System.out.println("--------------------------------------Before Updation--------------------------------------");
+		employeeList.stream().filter(e -> e.getSalary() > 30000)
+			.forEach(e -> printEmployeeDetails(e));
+		
+		List<Employee> employeeTempList = employeeList.stream().filter(e -> e.getSalary() > 30000)
+		.map(e -> {
+				e.setDepartment("Manager");
+				return e;
+			})
+		.collect(Collectors.toList());
+		
+		System.out.println("--------------------------------------After Updation--------------------------------------");
+		employeeTempList.stream()
+		.forEach(e -> printEmployeeDetails(e));
+	}
+	
+	public void topPaidEmployeeByDepartment() {
+		employeeList.stream()
+			.collect(Collectors.groupingBy(Employee::getDepartment,
+					Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary)), Optional::get)))
+			.entrySet().forEach(entry -> {
+				System.out.println(entry.getKey());
+				System.out.println("----------------------------------------------------------------");
+				printEmployeeDetails(entry.getValue());
 			});
 	}
 
--- functionalprogramming/src/com/aswaraj/streams/EmployeeTest.java
+++ functionalprogramming/src/com/aswaraj/streams/EmployeeTest.java
@@ -1,14 +1,16 @@
 package com.aswaraj.streams;
 
-import java.util.List;
+import java.util.Scanner;
 
 public class EmployeeTest {
 
 	public static void main(String[] args) {
 		EmployeeService employeeService = new EmployeeService();
 		employeeService.addEmployee();
-		int option = 7;
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter option:");
+		int option = sc.nextInt();
+
 		switch (option) {
 		case 1:
 			employeeService.countByGender();
@@ -18,14 +20,40 @@ public class EmployeeTest {
 			break;
 		case 3:
 			employeeService.printAvgAgeByGender();
+			break;
 		case 4:
 			employeeService.getHighestPaidEmployee();
+			break;
 		case 5:
 			employeeService.filterEmployeeByJoiningDate();
+			break;
 		case 6:
 			employeeService.countByDepartment();
+			break;
 		case 7:
 			employeeService.printAvgSalaryByDepartment();
+			break;
+		case 8:
+			employeeService.filterYoungestEmployee();
+			break;
+		case 9:
+			employeeService.seniorMostEmployee();
+			break;
+		case 10:
+			employeeService.employeesByDepartments();
+			break;
+		case 11:
+			employeeService.averageSalaryByGender();
+			break;
+		case 12:
+			employeeService.getAllEmployeesByDepartment();
+			break;
+		case 13:
+			employeeService.updateEmployeeDepartment();
+			break;
+		case 14:
+			employeeService.topPaidEmployeeByDepartment();
+			break;
 		default:
 			break;
 		}
